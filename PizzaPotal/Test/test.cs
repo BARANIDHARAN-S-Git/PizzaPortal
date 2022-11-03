f25415a5063ae9e28fcfab09454e9227d6e26b23
BARANIDHARAN-S-Git <barani001cdm@gmail.com>
2022-11-03 18:49:56 +0530

    PizzaPortal

---
+++ PizzaPotal/Test/test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework.Internal.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PizzaPotal;
+using PizzaPotal.Models;
+
+namespace Test
+{
+    [TestFixture]
+    public class test
+    {
+        pizzatype p = null;
+        public test()
+        {
+            p = new pizzatype();
+        }
+
+        [TestCase(ExpectedResult =6)]
+        [TestCase(ExpectedResult =7)]
+        public int check(int qnty)
+        {
+
+            p.add();
+            return (p.pizza.Count);
+            
+        }
+    }
+}
